29aa961cbcf719c100a66c32adeefd7e8d9e3505
JuliaCaldeira <julia.pcfranca@gmail.com>
2022-11-19 14:11:34 -0300

    cards logic

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ let characters = ['./images/tanjiro.jpg',
     './images/muzan.jpg',
     './images/inosuke.jpg',
     './images/zenitsu.jpg'];
-let cube;
-let count = 0;
-let cubes = [];
+let img_card;
+let count = 1;
+let img_cards = [];
 let visibility = 0;
+const back = document.getElementById('background');
 
 function addElement() {
-    let back = document.getElementById('background');
-    let divStart = document.getElementById('ButtonBackground');
-    let imgs = [];
-    let imgs_srcs = [];
+    const divStart = document.getElementById('ButtonBackground');
+    const imgs = [];
+    const imgs_srcs = [];
 
     back.removeChild(button);
     back.removeChild(divStart);
@@ -25,57 +25,53 @@ function addElement() {
         imgs_srcs.push(characters[i]);
     }
 
-    for(let i=0;i<10;i++) {
+    for(let i=0;i<characters.length*2;i++) {
         let img = new Image();
         img.src = imgs_srcs[i];
-        img.width = 200;
-        img.height = 200;
+        img.id = 'char_imgs';
         imgs[i] = img;
     }
 
     shuffleArray(imgs);
     
-    for(let i=0;i<10;i++) {
-        let div = document.createElement('div');
+    for(let i=0;i<characters.length*2;i++) {
+        const div = document.createElement('div');
         div.className = 'card';
         div.id = i;
 
         back.appendChild(div);
 
-        div.addEventListener("click", function(){round(div.id)}, false);
+        div.addEventListener("click", function(){showCard(div.id)}, false);
 
-        cube = document.createElement('div');
-        cube.className = 'cube';
-        cube.id = 'cube'+i;
+        img_card = document.createElement('div');
+        img_card.className = 'imgCard';
+        img_card.id = 'img_card'+i;
 
-        cube.appendChild(imgs[i]);
+        img_card.appendChild(imgs[i]);
 
-        div.appendChild(cube);
+        div.appendChild(img_card);
 
     }
-
-    const timer = document.createElement('div');
-    timer.id = 'timer';
-    timer.textContent = '00:00';
-    back.appendChild(timer);
-    
-
     
 }
 
-function round(id) {
-    count++;
-    if (count < 3) {
-        const divCube = document.getElementById('cube'+id);
-        divCube.style.visibility = 'visible';
-        cubes.push(divCube.id);
-    }
-    if (count == 2) {
-        verify(cubes);
-    }
+function showCard(id) {
+    if(count < 3) {
+        const divImgCard = document.getElementById('img_card'+id);
+        divImgCard.style.visibility = 'visible';
+        img_cards.push(divImgCard.id);
+    };
+
+    if (img_cards[0] != img_cards[1]) {
+        if (count == 2) {
+                verify(img_cards);
+            }
+        count++;
+    } else {
+        img_cards.shift();
+    };
     
-    if(visibility == 5) {
-        //timer(false);
+    if(visibility == characters.length) {
         end();
     }
 
@@ -88,34 +84,28 @@ function shuffleArray(inputArray){
 function verify(cubeHTML) {
    let img1 = document.getElementById(cubeHTML[0]).childNodes[0].currentSrc;
    let img2 = document.getElementById(cubeHTML[1]).childNodes[0].currentSrc;
-   console.log(img1, img2);
    if (img1 == img2) {
-    count = 0;
-    cubes = [];
+    count = 1;
+    img_cards = [];
     visibility++;
    } else {
     setTimeout(function() {
         document.getElementById(cubeHTML[0]).style.visibility = 'hidden';
         document.getElementById(cubeHTML[1]).style.visibility = 'hidden';
-        count = 0;
-        cubes = [];
+        count = 1;
+        img_cards = [];
     }, 500);
    }
 }
 
 function end() {
-    for(let i=0;i<10;i++) {
+    for(let i=0;i<characters.length*2;i++) {
         const id = document.getElementById(i);
         id.parentNode.removeChild(id);
     }
-    const back = document.getElementById('background');
-    let endDiv = document.createElement('div');
-    endDiv.id = 'endScreen';
-    endDiv.textContent = 'Parabéns! Você ganhou!';
     let btn = document.createElement('button');
     btn.id = 'restartBtn';
     btn.textContent = 'Jogar de novo';
-    back.appendChild(endDiv);
     back.appendChild(btn);
 }
 
